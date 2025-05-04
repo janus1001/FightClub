f81c3eb2ebfcdbff0475bcb2b55dca5883c4529e
marcinoo7 <marcinwolanski.fa@gmail.com>
2025-05-04 20:42:04 +0200

    Selecting multiple units works

--- Assets/UnitManager.cs
+++ Assets/UnitManager.cs
@@ -1,23 +1,42 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UnitManager : MonoBehaviour
 {
-    private AgentMovement selectedUnit;
-    private AgentMovement SelectedUnit
+    public RectTransform selectionPanelTransform;
+    
+    private bool isHoldingSelection;
+    private Vector2 startSelectionPosition;
+    private Vector2 currentSelectionPosition;
+    
+    
+    private List<AgentMovement> selectedUnits = new List<AgentMovement>();
+    private List<AgentMovement> SelectedUnits
     {
         get
         {
-            return selectedUnit;
+            return selectedUnits;
         }
         set
         {
-            selectedUnit?.SetOutline(false);
-            selectedUnit = value;
+            selectedUnits.ForEach(unit => unit.SetOutline(false));
+            selectedUnits = value;
         }
     }
     
     private void Update()
     {
+        if (isHoldingSelection)
+        {
+            UpdateSelectionVisual();
+            
+            if (Input.GetMouseButtonUp(0))
+            {
+                FinishSelectingUnits();
+            }
+        }
+        
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -27,28 +46,81 @@ public class UnitManager : MonoBehaviour
                 AgentMovement unit = hit.collider.GetComponent<AgentMovement>();
                 if (unit != null)
                 {
+                    UnselectAll();
                     SelectUnit(unit);
                 }
             }
             else if (Physics.Raycast(ray, out hit, 1000, LayerMask.GetMask("Default")))
             {
-                SelectedUnit.SetTarget(hit.point);
+                SelectedUnits.ForEach(unit => unit.SetTarget(hit.point));
+            }
+            else
+            {
+                UnselectAll();
+                StartSelectingUnits();
             }
         }
 
         if (Input.GetMouseButtonDown(1))
         {
-            SelectedUnit = null;
+            UnselectAll();
         }
+    }
+
+    private void UpdateSelectionVisual()
+    {
+        currentSelectionPosition = Input.mousePosition;
+
+        var xMin = Mathf.Min(startSelectionPosition.x, currentSelectionPosition.x);
+        var yMin = Mathf.Min(startSelectionPosition.y, currentSelectionPosition.y);
+        var xMax = Mathf.Max(startSelectionPosition.x, currentSelectionPosition.x);
+        var yMax = Mathf.Max(startSelectionPosition.y, currentSelectionPosition.y);
+
+        var topLeft = new Vector2(xMin, yMin);
+        var bottomRight = new Vector2(xMax, yMax);
+        
+        selectionPanelTransform.offsetMin = topLeft;
+        selectionPanelTransform.offsetMax = bottomRight;
+    }
+
+    private void StartSelectingUnits()
+    {
+        isHoldingSelection = true;
+        startSelectionPosition = Input.mousePosition;
+    }
+
+    private void FinishSelectingUnits()
+    {
+        isHoldingSelection = false;
+        selectionPanelTransform.offsetMin = Vector2.zero;
+        selectionPanelTransform.offsetMax = Vector2.zero;
+        var agentsAll = GameObject.FindObjectsByType<AgentMovement>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+        foreach (var agent in agentsAll)
+        {
+            var screenAgentPosition = Camera.main.WorldToScreenPoint(agent.transform.position);
+
+            if (screenAgentPosition.x > Mathf.Min(currentSelectionPosition.x, startSelectionPosition.x) && 
+                screenAgentPosition.x < Mathf.Max(currentSelectionPosition.x, startSelectionPosition.x) &&
+                screenAgentPosition.y > Mathf.Min(currentSelectionPosition.y, startSelectionPosition.y) &&
+                screenAgentPosition.y < Mathf.Max(currentSelectionPosition.y, startSelectionPosition.y))
+            {
+                SelectUnit(agent);
+            }
+        }
+    }
+
+    private void UnselectAll()
+    {
+        SelectedUnits.ForEach(unit => unit.SetOutline(false));
+        SelectedUnits.Clear();
     }
 
     private void SelectUnit(AgentMovement newUnit)
     {
-        SelectedUnit = newUnit;
-        
-        if (SelectedUnit != null)
+        if (newUnit != null)
         {
-            SelectedUnit.SetOutline(true);
+            SelectedUnits.Add(newUnit);
+            newUnit.SetOutline(true);
         }
     }
 }
